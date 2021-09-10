de2e58b7c0ce65151f06ab3881522cfbe1489824
Luke Hufnagle <leh17068@gmail.com>
2021-09-10 15:30:20 -0400

    Added modularization

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,67 @@
+from flask_app import app
+from flask import render_template, redirect, request, session
+from flask_app.models.user import User
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/show_users')
+def show_users():
+    users = User.select_all_users()
+    return render_template('show.html', users = users)
+
+
+@app.route('/create_user', methods=["POST"])
+def create_new_user():
+    data = {
+        'fname': request.form['fname'],
+        'lname': request.form['lname'],
+        'email': request.form['email']
+    }
+    User.create_user(data)
+    return redirect('/show_users')
+
+@app.route('/show_one_user/<id>')
+def one_user(id):
+    data = {
+        'user_id' : id
+    }
+    one_user = User.one_user(data)
+    print(one_user)
+    return render_template('one_user.html', user = one_user)
+
+@app.route('/show_update_user/<id>')
+def show_update_user_page(id):
+    data = {
+        'user_id' : id
+    }
+    show_update_user = User.show_update_user_page(data)
+    return render_template('update_user.html', user = show_update_user)
+
+@app.route('/update_user/<id>', methods=['POST'])
+def update_user(id):
+    data = {
+        'user_id' : id,
+        'fname': request.form['fname'],
+        'lname': request.form['lname'],
+        'email': request.form['email']
+    }
+    User.update_user(data)
+    return redirect('/show_users')
+
+@app.route('/delete_user/<id>', methods=['POST'])
+def delete_user(id):
+    data = {
+        'user_id' : id
+    }
+    User.delete_user(data)
+    return redirect('/show_users')
+
+@app.route('/return_to_create')
+def return_to_create():
+    return render_template('index.html')
+
+@app.route('/return_to_users')
+def return_to_users():
+    return redirect('/show_users')
--- server.py
+++ server.py
@@ -1,71 +1,7 @@
-from flask import Flask, render_template, redirect, request, session
-from user import User
-app = Flask(__name__)
-app.secret_key = "Its a secret"
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/show_users')
-def show_users():
-    users = User.select_all_users()
-    return render_template('show.html', users = users)
+from flask_app import app
+from flask_app.controllers import users
 
 
-@app.route('/create_user', methods=["POST"])
-def create_new_user():
-    data = {
-        'fname': request.form['fname'],
-        'lname': request.form['lname'],
-        'email': request.form['email']
-    }
-    User.create_user(data)
-    return redirect('/show_users')
-
-@app.route('/show_one_user/<id>')
-def one_user(id):
-    data = {
-        'user_id' : id
-    }
-    one_user = User.one_user(data)
-    print(one_user)
-    return render_template('one_user.html', user = one_user)
-
-@app.route('/show_update_user/<id>')
-def show_update_user_page(id):
-    data = {
-        'user_id' : id
-    }
-    show_update_user = User.show_update_user_page(data)
-    return render_template('update_user.html', user = show_update_user)
-
-@app.route('/update_user/<id>', methods=['POST'])
-def update_user(id):
-    data = {
-        'user_id' : id,
-        'fname': request.form['fname'],
-        'lname': request.form['lname'],
-        'email': request.form['email']
-    }
-    User.update_user(data)
-    return redirect('/show_users')
-
-@app.route('/delete_user/<id>', methods=['POST'])
-def delete_user(id):
-    data = {
-        'user_id' : id
-    }
-    User.delete_user(data)
-    return redirect('/show_users')
-
-@app.route('/return_to_create')
-def return_to_create():
-    return render_template('index.html')
-
-@app.route('/return_to_users')
-def return_to_users():
-    return redirect('/show_users')
 
 if __name__=='__main__':
     app.run(debug=True)

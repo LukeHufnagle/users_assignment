26b2f95e78150cd680a23eec1d2081574eec2304
Luke Hufnagle <leh17068@gmail.com>
2021-09-10 14:53:20 -0400

    Updated User database

--- server.py
+++ server.py
@@ -23,8 +23,49 @@ def create_new_user():
     User.create_user(data)
     return redirect('/show_users')
 
+@app.route('/show_one_user/<id>')
+def one_user(id):
+    data = {
+        'user_id' : id
+    }
+    one_user = User.one_user(data)
+    print(one_user)
+    return render_template('one_user.html', user = one_user)
 
+@app.route('/show_update_user/<id>')
+def show_update_user_page(id):
+    data = {
+        'user_id' : id
+    }
+    show_update_user = User.show_update_user_page(data)
+    return render_template('update_user.html', user = show_update_user)
 
+@app.route('/update_user/<id>', methods=['POST'])
+def update_user(id):
+    data = {
+        'user_id' : id,
+        'fname': request.form['fname'],
+        'lname': request.form['lname'],
+        'email': request.form['email']
+    }
+    User.update_user(data)
+    return redirect('/show_users')
+
+@app.route('/delete_user/<id>', methods=['POST'])
+def delete_user(id):
+    data = {
+        'user_id' : id
+    }
+    User.delete_user(data)
+    return redirect('/show_users')
+
+@app.route('/return_to_create')
+def return_to_create():
+    return render_template('index.html')
+
+@app.route('/return_to_users')
+def return_to_users():
+    return redirect('/show_users')
 
 if __name__=='__main__':
     app.run(debug=True)
--- user.py
+++ user.py
@@ -20,3 +20,21 @@ class User:
         for user in results:
             users.append( cls(user) )
         return users
+    @classmethod
+    def one_user(cls, data):
+        query = 'SELECT * FROM users WHERE id = %(user_id)s;'
+        results = connectToMySQL('users').query_db(query, data)
+        return cls( results[0] )
+    @classmethod
+    def show_update_user_page(cls, data):
+        query = 'SELECT * FROM users WHERE id = %(user_id)s;'
+        results = connectToMySQL('users').query_db(query, data)
+        return cls( results[0] )
+    @classmethod
+    def update_user(cls, data):
+        query = 'UPDATE users SET first_name = %(fname)s, last_name = %(lname)s, email = %(email)s WHERE id = %(user_id)s;'
+        return connectToMySQL('users').query_db(query, data)
+    @classmethod
+    def delete_user(cls, data):
+        query = 'DELETE FROM users WHERE id = %(user_id)s'
+        return connectToMySQL('users').query_db(query, data)
